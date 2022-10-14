be8685c31d159ae08be49dd839ee80da010a7a7d
Ramon Giovani <ramongiovanii12@gmail.com>
2022-10-14 18:33:25 -0300

    Requisito 18 completo

--- script.js
+++ script.js
@@ -1,18 +1,32 @@
 // Ramon G B S Requisito 3
 
-const buttonEnter = document.querySelector('.button');
+const buttonEnter = document.querySelector(".button");
 
 const verifyTryber = () => {
-  const email = document.querySelector('#email').value;
-  const password = document.querySelector('#password').value;
-  if (email === 'tryber@teste.com' && password === '123456') {
-    alert('Olá, Tryber!');
+  const email = document.querySelector("#email").value;
+  const password = document.querySelector("#password").value;
+  if (email === "tryber@teste.com" && password === "123456") {
+    alert("Olá, Tryber!");
   }
-  alert('Email ou senha inválidos.');
+  alert("Email ou senha inválidos.");
 };
 
-buttonEnter.addEventListener('click', verifyTryber);
+buttonEnter.addEventListener("click", verifyTryber);
 
 // --------------------------------------------------
 
-const buttonCheck = document.getElementById('submit-btn');
+const checkbox = document.getElementById("agreement");
+const buttonCheck = document.getElementById("submit-btn");
+buttonCheck.disabled = true;
+
+// pedro E. Requesito 18
+function verifyCheck() {
+  const checkboxVerify = document.getElementById("agreement").value;
+  console.log(checkboxVerify);
+  if (buttonCheck.disabled === true) {
+    return (buttonCheck.disabled = false);
+  }
+  return (buttonCheck.disabled = true);
+}
+
+checkbox.addEventListener("click", verifyCheck);
