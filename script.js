3ff10cced608d0fc528a867cc906a01dccd530fc
Ramon Giovani <ramongiovanii12@gmail.com>
2022-10-14 15:46:41 -0300

    Requisito 3 e 4 completos

---
+++ script.js
@@ -0,0 +1,16 @@
+// Ramon G B S Requisito 3
+
+const buttonEnter = document.querySelector('.button');
+
+const verifyTryber = () => {
+  const email = document.querySelector('#email').value;
+  const password = document.querySelector('#password').value;
+  if (email === 'tryber@teste.com' && password === '123456') {
+    alert('Olá, Tryber!');
+  }
+  alert('Login ou senha inválidos.');
+};
+
+buttonEnter.addEventListener('click', verifyTryber);
+
+// --------------------------------------------------
