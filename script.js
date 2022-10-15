49098aa6f73694cd7e3c8600bbbd25cc3a6e741e
Ramon Giovani <ramongiovanii12@gmail.com>
2022-10-14 22:22:06 -0300

    Requisito 21 completo - Projeto finalizado

--- script.js
+++ script.js
@@ -34,11 +34,58 @@ checkbox.addEventListener('click', verifyCheck);
 
 // --------------------------------------------------
 
-// pedro E. Requesito 21 - bonus
+// pedro E. Requesito 20 - bonus
 const spanCounter = document.getElementById('counter');
 const textarea = document.getElementById('textarea');
 textarea.addEventListener('input', () => {
   const counter = 500 - textarea.value.length;
   spanCounter.innerText = counter;
-  console.log(spanCounter.innerText);
+});
+
+// Ramon G B S Requisito 21 - bonus
+const submitButton = document.querySelector('#submit-btn');
+
+const formCreator = () => {
+  const form = document.createElement('form');
+  form.setAttribute('id', 'form-data');
+  document.body.appendChild(form);
+  return form;
+};
+formCreator();
+
+const rescueInformation = () => {
+  const name = document.querySelector('#input-name').value;
+  const lastName = document.querySelector('#input-lastname').value;
+  const email = document.querySelector('#input-email').value;
+  const house = document.querySelector('#house').value;
+  const family = document.querySelector('input[name="family"]:checked').value;
+  const content = document.querySelector('#textarea').value;
+  const rate = document.querySelector('input[name="rate"]:checked').value;
+  const allValue = [name, lastName, email, house, family, content, rate];
+  return allValue;
+};
+
+const formConstructorFirst = () => {
+  const allValue = rescueInformation();
+  const form = document.querySelector('#form-data');
+  form.innerHTML = `<p>Nome: ${allValue[0]} ${allValue[1]}</p><p>Email: ${allValue[2]}</p>`;
+  const formArray = [];
+  for (let i = 0; i < 6; i += 1) {
+    if (document.querySelectorAll('.subject')[i].checked) {
+      formArray.push(document.querySelectorAll('.subject')[i].value);
+    }
+  }
+  form.innerHTML += `<p>Casa: ${allValue[3]}</p>`;
+  form.innerHTML += `<p>Família: ${allValue[4]}</p>`;
+  form.innerHTML += `<p>Matérias: ${formArray.toString().replaceAll(',', ', ')}</p>`;
+  form.innerHTML += `<p>Avaliação: ${allValue[6]}</p>`;
+  form.innerHTML += `<p>Observações: ${allValue[5]}</p>`;
+};
+
+submitButton.addEventListener('click', (event) => {
+  event.preventDefault();
+  const main = document.querySelector('main');
+  main.style.display = 'none';
+  rescueInformation();
+  formConstructorFirst();
 });
